93136bda2d23d79e0812c829982d3869a1d62349
Alexander Thomsen <thomsen81@gmail.com>
2026-03-03 21:25:39 +0100

    more progress in ex5

--- lecture_5/ex2/LinkedList.cpp
+++ lecture_5/ex2/LinkedList.cpp
@@ -1,4 +1,6 @@
 #include "LinkedList.h" 
+#include <stdexcept>
+#include <iostream>
 
 LinkedList::LinkedList() : size_(0)
 {
@@ -13,12 +15,59 @@ void LinkedList::insert_front(const cat& obj)
 }
 LinkedList::~LinkedList()
 {
+    /*methodology - it very badly worded in the excersize
+    head -> [A] -> [B] -> [C] -> null
+    we must get where A is point to (B) before we delete A, we can do this
+    until a node is pointing to NULL
+    */
+
+    // Link *p = &head; 
     Link *p = head.get_next();
-    if(size_ > 0)
+
+    while(p != nullptr)
     {
-        while(p->get_next() != nullptr)
-        {
-            
+        Link* p_next_link = p->get_next();
+        delete p;
+        p = p_next_link;
+    }   
+    head.set_next(nullptr);
+}
+
+void LinkedList::delete_front(void)
+{
+    Link* to_delete = this->head.get_next();
+    if(to_delete == nullptr)
+    {
+        return; // we cannot delete the void
+    }
+    else
+    {
+        Link *new_next = to_delete->get_next();
+        this->head.set_next(new_next);
+        delete to_delete;
+        this->size_--;    
+    }
+}
+Link* LinkedList::get(int index)
+{
+    try{
+        if(index < 0 || index >= size_){
+            throw std::out_of_range("out of range");
         }
+        Link* search = head.get_next();
+        
+        for(int i = 0; i < index; i++)
+        {   
+            //this essentially iterates through the list untill we arrive at the index
+            search = search->get_next();
+        }
+    
+        return search;
+
+
+    }
+    catch(std::out_of_range e)
+    {
+        std::cout << e.what()<< std::endl;
     }
 }
--- lecture_5/ex2/LinkedList.h
+++ lecture_5/ex2/LinkedList.h
@@ -10,14 +10,14 @@ private:
 public:
     LinkedList();
     ~LinkedList();
-    Link head; //implemented as composition and not pointer!
+    Link head; //implemented as composition and not pointer! (SENTINEL HEAD)
     void insert_front(const cat& obj);
-    void delete_front();
+    void delete_front(void);
 
     void insert_after(const cat& obj, Link& link);
     void delete_after();
 
-    Link get(int index);
+    Link* get(int index);
 
     const int size(void) const {return this->size_;}
 
