ddfd933eebc6f9d30efe70c8bb467da156ea9d41
Alexander Thomsen <thomsen81@gmail.com>
2026-04-15 23:08:21 +0200

    ex15 done

--- lecture_10/ex14/main.cpp
+++ lecture_10/ex14/main.cpp
@@ -3,11 +3,11 @@
 #include "sum.h"
 #include <vector>
 
-#define OP_NR 1
+#define OPG_NR 15
 
 int main(void)
 {
-#if OP_NR == 1
+#if OPG_NR == 14
     vect v1(1.0, 2.0);
     vect v2(2.0, 3.0);
     vect v3(4.0, 5.0);
@@ -26,6 +26,20 @@ int main(void)
     vect v5 = sum<vect>(it_start, it_end, vect{0,0});
 
     std::cout << v5.to_string() << std::endl;
+
+#elif OPG_NR == 15
+    /*iterator traits extracts value type from whatever iterator
+    is passed unto it*/
+    /*the return type is determined at compile time from the
+    iterator that is passed into it*/
+
+    std::vector<int> myVect2 = {1,5,4,76,78,2,3,4};
+
+    std::vector<int>::iterator being = myVect2.begin();
+    std::vector<int>::iterator end = myVect2.end();
+
+    int val = sum_2(being, end, 0);
+    std::cout << "val: " << val << std::endl;
 
 
 #endif
--- lecture_10/ex14/sum.h
+++ lecture_10/ex14/sum.h
@@ -16,3 +16,15 @@ T sum(typename std::vector<T>::iterator begin, typename std::vector<T>::iterator
     return res;
 }
 
+
+template<typename Iterator>
+typename std::iterator_traits<Iterator>::value_type
+sum_2(Iterator begin, Iterator end, typename std::iterator_traits<Iterator>::value_type initial_sum)
+{
+    for(; begin != end; ++begin)
+    {
+        initial_sum += *begin;
+    }
+    return initial_sum;
+}
+
